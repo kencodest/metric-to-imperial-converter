022773e55abde931491a619bf273ec657a7aa97c
Charlotte <81417641+Cuzozzi@users.noreply.github.com>
2022-06-06 14:02:55 +0200

    Update to index.js variable declarations

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-let userInput = document.getElementById("user-input")
-let calcLength = document.getElementById("calc-length")
-let calcVolume = document.getElementById("calc-volume")
-let calcMass = document.getElementById("calc-mass")
+const userInput = document.getElementById("user-input")
+const calcLength = document.getElementById("calc-length")
+const calcVolume = document.getElementById("calc-volume")
+const calcMass = document.getElementById("calc-mass")
 
 function conversion() {
     let userInputValue = userInput.value
-    let meterResult = userInputValue * 3.28084
-    let feetResult = userInputValue * 0.3048
-    let literResult = userInputValue * 0.219969
-    let gallonResult = userInputValue * 4.54609
-    let kiloResult = userInputValue * 2.20462
-    let poundResult = userInputValue * 0.453592
+    const meterResult = userInputValue * 3.28084
+    const feetResult = userInputValue * 0.3048
+    const literResult = userInputValue * 0.219969
+    const gallonResult = userInputValue * 4.54609
+    const kiloResult = userInputValue * 2.20462
+    const poundResult = userInputValue * 0.453592
     
     calcLength.textContent = `${userInputValue} meters = ${meterResult.toFixed(3)} feet | ${userInputValue} feet = ${feetResult.toFixed(3)} meters`
     
